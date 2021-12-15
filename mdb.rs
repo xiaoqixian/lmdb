f52785122aec208eae06d5562ef0c81a15a5c06b
xiaoqixian <2100125584@qq.com>
2021-12-15 22:34:04 +0800

    the first step is always the hardest

--- mdb.rs
+++ mdb.rs
@@ -7,10 +7,35 @@
   > Copyright@ https://github.com/xiaoqixian
  **********************************************/
 
-struct Db {
-    
+use std::sync::{Arc};
+use std::fs::{File, OpenOptions};
+use memmap::{*};
+
+type pageno_t = usize;
+type ptr = [u8];
+
+struct Val {
+    size: usize,
+    data: ptr
+}
+
+struct DBHead {
+    version: usize,
+    page_size: usize, ///os memory page size, in C, got by sysconf(_SC_PAGE_SIZE)
+}
+
+struct DB {
+    md_root: pageno_t,
+    cmp_func: fn(v1: Val, v2: Val) -> i32,
+    db_head: DBHead
 }
 
 struct Env {
+    fd: Arc<File>,
+    mmap: MmapMut,
+    w_txn: Txn, //current write transaction
+}
+
+struct Txn {
     
 }
