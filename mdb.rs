1cbe6411c15c76360b2e854136f61632336a67b8
xiaoqixian <2100125584@qq.com>
2021-12-15 17:13:30 +0800

    mdb.rs

---
+++ mdb.rs
@@ -0,0 +1,16 @@
+/**********************************************
+  > File Name		: mdb.rs
+  > Author		    : lunar
+  > Email			: lunar_ubuntu@qq.com
+  > Created Time	: Wed 15 Dec 2021 04:08:42 PM CST
+  > Location        : Shanghai
+  > Copyright@ https://github.com/xiaoqixian
+ **********************************************/
+
+struct Db {
+    
+}
+
+struct Env {
+    
+}
